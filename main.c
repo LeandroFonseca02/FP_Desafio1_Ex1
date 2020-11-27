73fa839db3ed541575e9f5e899d3407603117905
Leandro Fonseca <74561242+leandrofonseca02@users.noreply.github.com>
2020-11-27 12:34:42 +0000

    Initial Commit

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
-//
-// Created by linux on 11/27/20.
-//
+/*
+Ismat - Engenheiria Informática 1º Ano
+Fundamentos de Programação - Prof. Fracisco Pereira
+Leandro Fonseca
+17/11/2020 - 20:52
+Resumo: Calcular soma e produto dos 10 primeiros numeros naturais
+Inputs:
+Outputs:
+ */
 #include <stdio.h>
 
-int main(){
-    printf("Bom dia Mundos");
+int main() {
+
+    int intSoma = 0;
+    int intProd = 1;
+    int i;
+
+    for (int i = 1; i < 11; ++i) {
+        intSoma += i;
+        intProd *= i;
+    }
+    printf("A soma dos 10 primeiros numeros naturais: %d", intSoma);
+    printf("\nO produto dos 10 primeiros numeros naturais: %d", intProd);
     return 0;
 }
